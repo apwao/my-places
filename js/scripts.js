4ca8e00960846a20940b119e841ebb08cc163861
Sowayi <sapwao@gmail.com>
2019-02-27 04:29:49 -0500

    Add function to display locations

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,23 @@ function Places(name,location,time,landmark,review){
   this.landmark = landmark;
   this.review = review;
 }
-Places.prototype.viewLocation = (function(){
+Places.prototype.viewLocation = function(){
   return this.name + "," + this.location;
-});
+};
 
 //User Logic
 $(document).ready(function(){
-  $(".well#places").submit(function(
-    var myName = $("#my-name").val();
-    var myLocation = $("#my-location").vall();
-    var mySeason = $("#time-season").val();
-    var myLandmark = $("#my-landmarks").val();
-    var myReview = $("#my-review").val();
-  ))
-})
+  $(".well form").submit(function(event){
+    event.preventDefault();
+    var myName = $("input#my-name").val();
+    var myLocation = $("input#my-location").val();
+    var mySeason = $("input#time-season").val();
+    var myLandmark = $("input#my-landmarks").val();
+    var myReview = $("input#my-review").val();
+
+    var newPlace = new Places(myName, myLocation, mySeason, myLandmark, myReview);
+    $(".well ul").append("<li><span class='display'>" + newPlace.viewLocation() + "</span></li>")
+
+    
+  });
+});
