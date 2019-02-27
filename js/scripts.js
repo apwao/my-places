f67f2bbc6ce4a9c0c0e7d5d308387a01580d2683
Sowayi <sapwao@gmail.com>
2019-02-27 03:16:47 -0500

    Add user logic to receive input from the user

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,16 @@ function Places(name,location,time,landmark,review){
   this.review = review;
 }
 Places.prototype.viewLocation = (function(){
-  return this.name + this.location;
+  return this.name + "," + this.location;
 });
+
+//User Logic
+$(document).ready(function(){
+  $(".well#places").submit(function(
+    var myName = $("#my-name").val();
+    var myLocation = $("#my-location").vall();
+    var mySeason = $("#time-season").val();
+    var myLandmark = $("#my-landmarks").val();
+    var myReview = $("#my-review").val();
+  ))
+})
